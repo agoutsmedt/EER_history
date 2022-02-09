bd610e5854833516603edba09c03abde743b6ba0
agoutsmedt <agoutsmedt@hotmail.fr>
2022-02-09 15:25:39 +0100

    test Alex

--- Script_paths_and_basic_objects_EER.R
+++ Script_paths_and_basic_objects_EER.R
@@ -37,10 +37,10 @@ for (p in github_list) {
 
 ######################### Paths ##########################################------------
 
-if (stringr::str_detect(getwd(), "goutsmedt")) {
+if (str_detect(getwd(), "goutsmedt")) {
   data_path <- "C:/Users/goutsmedt/Mon Drive/data"
 } else {
-  if (string::str_detect(getwd(), "shortcut")) {
+  if (str_detect(getwd(), "shortcut")) {
     data_path <- "G:/.shortcut-targets-by-id/1EHqA0pp2uozTykWv0_Stf5xyrvo_dkN5/data"
   } else {
     data_path <- "/projects/data"
@@ -52,10 +52,11 @@ macro_AA_data <- here(data_path,
 eer_data <- here(data_path, 
                  "EER")
 
-picture_path <- here("EER_Paper", "Pictures")
-
 boards_path <- here(eer_data, "editorial_boards")
-macro_data <- here(data_path, "Corpus_Econlit_Matched_WoS")
+macro_data <- here(macro_AA_data, "Corpus_Econlit_Matched_WoS")
+
+picture_path <- here("EER_Paper", 
+                     "Pictures")
 
 ######################### data ##########################################------------
 
